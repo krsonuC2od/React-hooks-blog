79c2c621112921ef73ef99eb8633df2fe77ab9a2
krsonuC2od <190101120062@cutm.ac.in>
2023-06-24 23:47:40 +0530

    blog web is ready to deploye

--- src/Component/PostDetail.js
+++ src/Component/PostDetail.js
@@ -1,8 +1,27 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import {useParams} from 'react-router-dom'
+import {firestore} from './firebase'
+import './index.css'
 
 function PostDetail() {
+  const [post ,setPosts] =useState({});
+  const {postId}=useParams();
+
+  useEffect(()=>{
+    firestore
+    .collection('posts')
+    .doc(postId)
+    .get()
+    .then((snapshot)=>{
+     console.log(snapshot.data())
+     setPosts(snapshot.data())
+    })
+  })
     return (
-      <div className="">
-      PostDetail
+      <div className="post-detail">
+      <h1>{post.title}</h1>
+      <p>{post.content}</p>
       </div>
     );
   }
